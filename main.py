c5dc43d19e9d304e1b9eabc70b10f7ecf9c5e652
Lee Kol <79585933+leeKol@users.noreply.github.com>
2022-08-18 11:23:53 +0300

    Server Setup.

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+from requests import Response
+import requests
+
+TOKEN = '5692289016:AAE5u76CPfvDUUtqBFhzVwgGlbVpT-DtUB4'
+TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url=http://9baa-2a02-6680-1109-2107-6d9d-bfe8-7df4-58ab.ngrok.io/message'.format(TOKEN)
+
+requests.get(TELEGRAM_INIT_WEBHOOK_URL)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def sanity():
+    return "Server is running"
+
+
+if __name__ == '__main__':
+    app.run(port=5002)
